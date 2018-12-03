5ba1e42b90e2a91430dcd2f4f4e056527c8de732
matias.ferreira <matias.ferreira@fing.edu.uy>
2018-12-02 23:02:59 -0300

    sz

--- src/Products.js
+++ src/Products.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-const API = "https://backpis.azurewebsites.net/api/tutorialCategories/published";
-
 let array = [
     ["Sándwich de jamón", "99", "99"],
     ["Sándwich olímpico", "170", "190"],
@@ -25,14 +23,15 @@ class Products extends Component {
         super(props);
         this.state = {
             loaded: false,
-            products: null
         };
     }
 
     componentDidMount() {
-        fetch(API)
-            .then(response => response.json())
-            .then(categories => this.setState({ categories }));
+        setTimeout(() => {
+            this.setState({
+                loaded: true
+            });
+        }, 3000);
     }
 
     render() {
@@ -40,32 +39,38 @@ class Products extends Component {
         return (
             <div className="uk-panel uk-container tm-block-top-a">
                 <h2 className="mod-panel-title">Productos recomendados</h2>
-                <div class="uk-child-width-1-2@s uk-child-width-1-4@m uk-text-center uk-grid-match" data-uk-grid>
-                    {
-                        array.map((item, index) =>
-                            <div className="uk-flex uk-flex-middle" key={index}>
-                                <div class="uk-card uk-card-default uk-card-body">
-                                    <div>{item[0]}</div>
-                                    {
-                                        item[1] < item[2]
-                                        ?
-                                            <div>
-                                                <div><strong>Pedidos Ya {"$ " + item[1]}</strong></div>
-                                                <div>Glovo {"$ " + item[2]}</div>
-                                            </div>
-                                        :
-                                            <div>
-                                                <div>Pedidos Ya {"$ " + item[1]}</div>
-                                                <div><strong>Glovo {"$ " + item[2]}</strong></div>
+                {
+                this.state.loaded ?
+                        <div className="uk-child-width-1-2@s uk-child-width-1-4@m uk-text-center uk-grid-match" data-uk-scrollspy="target: > div; cls:uk-animation-slide-right; delay: 250" data-uk-grid>
+                        {
+                            array.map((item, index) =>
+                                <div className="uk-flex uk-flex-middle" key={index}>
+                                    <div className="uk-card uk-card-default uk-card-body">
+                                        <div>{item[0]}</div>
+                                        {
+                                            item[1] < item[2]
+                                            ?
+                                                <div>
+                                                    <div><strong>Pedidos Ya {"$ " + item[1]}</strong></div>
+                                                    <div>Glovo {"$ " + item[2]}</div>
+                                                </div>
+                                            :
+                                                <div>
+                                                    <div>Pedidos Ya {"$ " + item[1]}</div>
+                                                    <div><strong>Glovo {"$ " + item[2]}</strong></div>
 
-                                            </div>
+                                                </div>
 
-                                    }
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-                        )
-                    }
+                            )
+                        }
+                    </div>
+                : <div className="uk-flex uk-flex-center">
+                    <div uk-spinner="ratio: 3"></div>
                 </div>
+                }
             </div>
         );
     }
